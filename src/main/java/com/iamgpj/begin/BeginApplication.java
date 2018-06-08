cb7cc97ac3b58e0038054b0ef36a87a7e583d004
heiyubai <iamgpj@163.com>
2018-06-08 20:32:26 +0800

    项目初始化

---
+++ src/main/java/com/iamgpj/begin/BeginApplication.java
@@ -0,0 +1,12 @@
+package com.iamgpj.begin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginApplication.class, args);
+    }
+}
